131c71cfbd85e39377dcd81449e901724e74eecb
PixelPoints-work <justinephriam9@gmail.com>
2024-10-10 19:54:15 -0700

    clean up

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,16 +58,12 @@ return {
 
       -- Set up C LSP (C)
       require('lspconfig').clangd.setup({lsp_opts})
-
-      -- Set up nim LSP (nim_langserver)
-      require('lspconfig').nim_langserver.setup({lsp_opts})
-
+      -- Ocaml lsp
+      require('lspconfig').ocamllsp.setup({lsp_opts})
       -- Deno LSP
-      require('lspconfig').denols.setup({lsp_opts})
-      -- Rust LSP
-      require('lspconfig').rust_analyzer.setup({lsp_opts})
+      -- require('lspconfig').denols.setup({lsp_opts})
       -- TS, HTML, CSS
-      -- require('lspconfig').ts_ls.setup(lsp_opts)
+      require('lspconfig').ts_ls.setup(lsp_opts)
       require('lspconfig').cssls.setup(lsp_opts)
       require('lspconfig').html.setup(lsp_opts)
       -- Completion setup
