f6bc23cd3babaa5f8815846b50a3ccca3528b8d5
JustinEphriam <justinephriam9@gmail.com>
2025-06-16 09:00:03 -0700

    theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
         require('lualine').setup({
 
             options = {
-                theme = 'tomorrow-night'
+                theme = 'jellybeans'
             }
         })
     end
