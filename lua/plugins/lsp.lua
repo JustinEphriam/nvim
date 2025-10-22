68c98e5c0220f2865f2e6393fdc252110795a84b
Justin Ephriam <justinephriam9@gmail.com>
2025-10-21 21:44:54 -0700

    Fixed deprecated lsp issue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -45,30 +45,53 @@ return {
         capabilities = require('cmp_nvim_lsp').default_capabilities(),
       }
 
-
-      require('lspconfig').c3_lsp.setup({
+      -- New vim.lsp.config API
+      vim.lsp.config('c3_lsp', {
         cmd = { 
           "c3lsp",
           "-c3c-path", "/usr/bin/c3c",
           "-diagnostics-delay", "1000"
         },
         filetypes = { "c3" },
-        root_dir = require("lspconfig.util").root_pattern("project.json"),
-        on_attach = lsp_attach,
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        root_dir = vim.fs.root(0, { "project.json" }),
       })
-      require('lspconfig').gleam.setup(lsp_opts)
-      require('lspconfig').zls.setup(lsp_opts)
-      -- require('lspconfig').ocamllsp.setup(lsp_opts)
-      require('lspconfig').gopls.setup(lsp_opts)
-      require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
-      require('lspconfig').clangd.setup(lsp_opts)
-      require('lspconfig').ols.setup(lsp_opts)
-      -- require('lspconfig').denols.setup(lsp_opts)
-      require('lspconfig').cssls.setup(lsp_opts)
-      require('lspconfig').html.setup(lsp_opts)
-      require('lspconfig').ts_ls.setup(lsp_opts)
-      require("lspconfig").roc_ls.setup({lsp_opts})
+      vim.lsp.enable('c3_lsp')
+
+      -- vim.lsp.config('gleam', {})
+      -- vim.lsp.enable('gleam')
+      
+      vim.lsp.config('zls', {})
+      vim.lsp.enable('zls')
+      
+      -- vim.lsp.config('ocamllsp', {})
+      -- vim.lsp.enable('ocamllsp')
+      
+      vim.lsp.config('gopls', {})
+      vim.lsp.enable('gopls')
+      
+      vim.lsp.config('lua_ls', lsp_zero.nvim_lua_ls())
+      vim.lsp.enable('lua_ls')
+      
+      vim.lsp.config('clangd', {})
+      vim.lsp.enable('clangd')
+      
+      vim.lsp.config('ols', {})
+      vim.lsp.enable('ols')
+      
+      -- vim.lsp.config('denols', {})
+      -- vim.lsp.enable('denols')
+      
+      vim.lsp.config('cssls', {})
+      vim.lsp.enable('cssls')
+      
+      vim.lsp.config('html', {})
+      vim.lsp.enable('html')
+      
+      vim.lsp.config('ts_ls', {})
+      vim.lsp.enable('ts_ls')
+      
+      vim.lsp.config('roc_ls', {})
+      vim.lsp.enable('roc_ls')
       
       local cmp = require('cmp')
       cmp.setup({
@@ -87,23 +110,18 @@ return {
   },
   {
     'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
+    version = '^5',
+    lazy = false,
     ft = { 'rust' },
     config = function()
       vim.g.rustaceanvim = {
-        -- Plugin configuration
         tools = {
-          -- Automatically set inlay hints (type hints)
-          -- There is an issue due to which the arguments are not shown
           inlay_hints = {
             auto = true,
           },
         },
-        -- LSP configuration
         server = {
           on_attach = function(client, bufnr)
-            -- Custom keymaps for Rust
             local opts = { buffer = bufnr, noremap = true, silent = true }
             vim.keymap.set('n', '<leader>ra', function()
               vim.cmd.RustLsp('hover', 'actions')
@@ -116,7 +134,6 @@ return {
             end, opts)
           end,
           default_settings = {
-            -- rust-analyzer language server configuration
             ['rust-analyzer'] = {
               checkOnSave = {
                 enable = true,
@@ -134,10 +151,7 @@ return {
             },
           },
         },
-        -- DAP configuration
-        dap = {
-          -- Configuration here
-        },
+        dap = {},
       }
     end,
   },
